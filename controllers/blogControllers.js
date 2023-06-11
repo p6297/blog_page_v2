b697fa938131b9ba30c17b609bb4851a7261d546
Partha Jyoti Pandey <104695799+p6297@users.noreply.github.com>
2023-06-11 09:31:05 +0530

    handled promises

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -1,75 +1,59 @@
-//blog_index, blog_details, blog_create_get , blog_create_post, blog_delete
 const Blog = require("../models/blog");
 
-
-const blog_index = async(req,res) => {
-    try {
-        const response = await Blog.find().sort({createdAt: -1});
-        res.render("blog",{title:"all blogs", blogs:response})
-        
-    } catch (error) {
-        throw error;
-        
-    }
-}
-
-const blog_details = async(req,res) => {
-    try {
-        const id = (req.params.id)
-        const result = await Blog.findById(id)
-        res.render("details",{title:"Blog Details", blog:result,})
-
-
-        
-    } catch (error) {
-        throw error;
-        
-    }
-}
-
-const blog_create_get = (req,res) => {
-    res.render("create",{title:"Create a new Blog"})
-}
-
-
-
-const blog_create_post = (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then((result) => {
-            res.redirect("/blogs");
-        })
-        .catch((err) => {
-           console.log(err);
-        });
+const blog_index = async (req, res) => {
+  try {
+    const response = await Blog.find().sort({ createdAt: -1 });
+    res.render("blog", { title: "All Blogs", blogs: response });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
+const blog_details = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await Blog.findById(id);
+    res.render("details", { title: "Blog Details", blog: result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
-const blog_delete = (req,res) => {
-    try {
-        const id = (req.params.id)
-        Blog.findByIdAndDelete(id)
-        .then((result)=> {
-            res.json({redirect: "/blogs"})
+const blog_create_get = (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
+};
 
-        })
-    } catch (error) {
-        throw error;
-        
-    }
+const blog_create_post = (req, res) => {
+  const blog = new Blog(req.body);
 
-}
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    });
+};
 
+const blog_delete = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Blog.findByIdAndDelete(id);
+    res.json({ redirect: "/blogs" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
-
-
-module.exports ={
-    blog_index,
-    blog_details,
-    blog_create_get,
-    blog_create_post,
-    blog_delete
-
-
-}
+module.exports = {
+  blog_index,
+  blog_details,
+  blog_create_get,
+  blog_create_post,
+  blog_delete,
+};
