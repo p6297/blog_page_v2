32633e258a0d365f462c7bb368cc355863b38a79
Partha Jyoti Pandey <104695799+p6297@users.noreply.github.com>
2023-06-11 09:36:10 +0530

    Update blogControllers.js

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -2,7 +2,7 @@ const Blog = require("../models/blog");
 
 const blog_index = async (req, res) => {
   try {
-    const response = await Blog.find().sort({ createdAt: -1 });
+    const response = await Blog.find().sort({ createdAt: -1 }).timeout(10000);
     res.render("blog", { title: "All Blogs", blogs: response });
   } catch (error) {
     console.error(error);
